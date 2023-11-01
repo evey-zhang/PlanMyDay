248526b3ae93617bab6e8f12c471266ae8876f73
chenterry85 <chenterry85@gmail.com>
2023-11-01 11:27:54 -0700

    merge with Google gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-maps:17.0.1")
+	implementation ("com.google.android.gms:play-services-location:21.0.1")
+
+	//Google Maps services
+	implementation("com.google.maps:google-maps-services:2.2.0")
+
+	implementation ("org.slf4j:slf4j-simple:1.7.25")
 }
